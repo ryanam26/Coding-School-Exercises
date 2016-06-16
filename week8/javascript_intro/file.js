8403f615a5aff5e63270a97e1db209baebca8e7c
Ryan Morrison <ryanam26@yahoo.com>
2016-06-15 21:59:09 -0400

    function practise

--- week8/javascript_intro/file.js
+++ week8/javascript_intro/file.js
@@ -5,15 +5,34 @@
 var numbers = "80:70:90:100";
  
  function avarageColon(string) {
- 	var array = string.split (":");
- 	console.log(array);
+ 	var array = string.split (":"); // turns string into an array
+ 
 
  	
- 	var add = array.reduce(function (prev, num) {
- 		return parseInt(prev) + parseInt(num);
+ 	var add = array.reduce(function (prev, num) {  // adds all the numbers in the array.
+ 		return parseInt(prev) + parseInt(num);     // converts it to an interger
  	})
- 	return (add/array.length); ///(array.length)
+
+ 	return (add/array.length); 
  }  
  
 console.log(avarageColon(numbers));
+
+//new code here_______________________________//
+
+var numbers = "90:40:50:60";
+
+function avarage(numbers) {
+	var array = numbers.split (":");
+
+	var add = array.reduce(function(prev,num){
+		return parseInt(prev) + parseInt(num);
+	})
+	return (add/array.length);
+}
+
+console.log(avarage(numbers));
+
+
+
 
