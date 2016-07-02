2dac46d04d36bcaebb23fb15d65d74e017e0b7fd
Ryan Morrison <ryanam26@yahoo.com>
2016-07-02 15:50:25 -0400

    added links

--- week9/timetracking/app/controllers/time_entries_controller.rb
+++ week9/timetracking/app/controllers/time_entries_controller.rb
@@ -1,14 +1,34 @@
 class TimeEntriesController < ApplicationController
+
+	 def new
+
+	 	@project = Project.find(params[:project_id])
+	 	@time_entry = @project.time_entries.new
+	 	@time
+	 end
+
+	def create
+		# @input: params[:project_id] and params[:time_entry]
+		@project = Project.find params[:project_id]
+		time_entry_params = params.require(:time_entry).permit(
+			:hours, :minutes, :date)
+		
+		@time_entry = @project.time_entries.new params[time_entry_params]
+
+			if @time_entry.save
+				redirect_to project_time_entries_path(project)
+			else
+				render 'new'
+			end
+
+	end
+
+
+
 	 def index
 	 	@project = Project.find(params[:project_id])
 	 	@time_entries = @project.time_entries
 	 end
 
-
-	 def create
-
-	 	@project = Project.find(params[:id])
-	 	@time_entry = @project.time_entries.new
-	 	@time
 end
 
--- week9/timetracking/app/models/time_entry.rb
+++ week9/timetracking/app/models/time_entry.rb
@@ -1,3 +1,5 @@
 class TimeEntry < ActiveRecord::Base
 	belongs_to :project
+	validates :project, presence: true
+	validates :hours, :minutes, presence: true, numericality: true
 end
