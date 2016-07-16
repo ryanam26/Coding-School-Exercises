14eed5a0432aa71eb7b83dde449e3373a34972e1
Ryan Morrison <ryanam26@yahoo.com>
2016-07-15 21:20:42 -0400

    add more buttons

--- js_tutorial/program.js
+++ js_tutorial/program.js
@@ -1,1 +1,30 @@
 document.writeln('Hello, world!');
+
+funtion btnplus() {
+	document.Calculator.display.style.value +="+";
+	document.Calculator.display.style.textAlign="right";
+};
+funtion btnsub() {
+	document.Calculator.display.style.value +="-";
+	document.Calculator.display.style.textAlign="right";
+};
+funtion btnmult() {
+	document.Calculator.display.style.value +="*";
+	document.Calculator.display.style.textAlign="right";
+};
+funtion btnmod() {
+	document.Calculator.display.style.value +="%";
+	document.Calculator.display.style.textAlign="right";
+};
+funtion btndot() {
+	document.Calculator.display.style.value +=".";
+	document.Calculator.display.style.textAlign="right";
+};
+funtion btndic() {
+	document.Calculator.display.style.value +="/";
+	document.Calculator.display.style.textAlign="right";
+};
+funtion btnclear() {
+	document.Calculator.display.style.value ="";
+	document.Calculator.display.style.textAlign="right";
+};
