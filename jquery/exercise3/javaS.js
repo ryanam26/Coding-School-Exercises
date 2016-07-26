be81de2f4d509d004fd62edf9a0327bde2e6f671
Ryan Morrison <ryanam26@yahoo.com>
2016-07-25 20:30:36 -0400

    exercise3

---
+++ jquery/exercise3/javaS.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+         
+                  $("button").click(function() {
+                   
+
+          $("#left").css("background-color", "yellow");
+          $("#center").css("background-color", "blue");
+          $("#right").css("background-color", "red");
+          $("#left").css("color", "red");
+          $("#center").css("color", "yellow");
+          $("#right").css("color", "blue");
+
+ 
+ 	});
+ });
