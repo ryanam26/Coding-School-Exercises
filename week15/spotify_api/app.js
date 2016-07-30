6f60b55ac6febcad22e82fce442356021a80391a
Ryan Morrison <ryanam26@yahoo.com>
2016-07-30 12:28:05 -0400

    completed Update

--- week15/spotify_api/app.js
+++ week15/spotify_api/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$('.js-artist-form').on("submit", function(event){
 		event.preventDefault();
 
@@ -10,7 +11,45 @@ $(document).ready(function(){
 			error: artistError,
 		})
 	})
+
+	$(".js-artist-list").on("click", ".artist-li", function(event){
+		var artistId = $(event.currentTarget).data("artist-id")
+		var artistName = $(event.currentTarget).data("artist-name")
+
+		$.ajax({
+			url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+			success: function(response){
+				showAlbums(response, artistName);
+			},
+			error: albumError
+		})
+		// Use data attribute to get artist id out of LI
+	})
+
 })
+
+function showAlbums(response, artistName){
+	$(".modal-artist-name").text(artistName);
+	var albums = response.items;
+	albums.forEach(appendAlbum);
+	$(".modal").modal("show");
+}
+
+
+function appendAlbum(album){
+	var html = `
+		<li class="album-li">
+			${album.name}
+		</li>
+	`
+
+	$('.modal-artist-name').append(html);
+}
+
+function albumError(err){
+	console.log(err);
+}
+
 
 function showArtists(response){
 	console.log("Response", response);
@@ -22,14 +61,14 @@ function showArtists(response){
 function appendArtist(artist){
 	var artistImage;
 
-	if (artist.images[2]){
-		artistImage = artist.images[2].url
+	if (artist.images[0]){
+		artistImage = artist.images[0].url
 	} else {
 		artistImage = "http://placehold.it/200x200"
 	}
 
 	var html = `
-		<li>
+		<li class="artist-li" data-artist-id=${artist.id} data-artist-name="${artist.name}">
 			<h3>${artist.name}</h3>
 			<img src=${artistImage}>
 		</li>
